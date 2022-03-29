d5271da83c0896664e071329b21abcbdfdb6938e
nerett <pt-mvb@inbox.lv>
2022-03-29 20:37:59 +0300

    modified run.sh to execute open.sh after a problem was found

--- run.sh
+++ run.sh
@@ -15,6 +15,8 @@ if [[ $? -eq 0 ]]; then
 
     str_num=$(echo $status | sed -nr "s/.*на странице №([[:digit:]]{1,4})!.*/\1/p")
     echo "Задача $zad найдена на странице №$str_num!"
+    
+    ./open.sh $sem $str_num $zad
 
 else
     echo "Задача $zad не найдена в корявнике:("
