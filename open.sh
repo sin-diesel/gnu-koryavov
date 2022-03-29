d9575853c9be9d711a016a80ed5dbc65634ac25e
nerett <pt-mvb@inbox.lv>
2022-03-29 20:40:24 +0300

    added download.sh and open.sh: digital version of koryavnik downloading and opening

---
+++ open.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sem=$1
+str_num=$2
+zad=$3
+
+#positive=("Y" "y" "Yes" "yes")
+
+echo -n "Открыть электронный корявник? (Д/н): "
+read ans
+    
+if [[ $ans == "y"* || $ans == "Y"* || $ans == "д"* || $ans == "Д"* ]]; then
+
+    if [ ! -f "KORYAVNIKS/${sem}.djvu" ]; then
+    
+        mkdir -p KORYAVNIKS/
+        ./download.sh $sem
+      
+    fi
+     
+     #here you can place your pdf viewer script
+    ./okular.sh $sem $str_num
+
+fi
