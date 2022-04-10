624de5c4800d9123c892eb35f0bead941a1aa27a
nerett <pt-mvb@inbox.lv>
2022-04-10 04:25:35 +0300

    added xreader koryavniks opening support

---
+++ xreader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+xreader --version 2> /dev/null
+if [[ $? -ne 0 ]]; then
+    
+    echo "Xreader не установлен!"
+    exit
+    
+fi
+
+sem=$1
+str_num=$2
+
+xreader -p $str_num ~/gnu-koryavov/KORYAVNIKS/${sem}.djvu
