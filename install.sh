5de28fbc78a504ac668ab7ddea18d6b8cd985a82
nerett <pt-mvb@inbox.lv>
2022-04-14 19:14:08 +0300

    changed dir structure, updated intall.sh script

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 home_dir="gnu-koryavov"
-read -p "Вы уверены, что хотите установить скрипт gnu-koryavov в систему? (Д/н): " ans
+gnukoryavov_name="test-gnu-koryavov"
 
+read -p "Вы уверены, что хотите установить скрипт gnu-koryavov в систему? (Д/н): " ans
 if [[ $ans == "y"* || $ans == "Y"* || $ans == "д"* || $ans == "Д"* ]]; then
 
     mkdir -p -v ~/$home_dir/KORYAVNIKS
     cp config.conf ~/$home_dir
-    sudo cp run.sh /usr/local/bin/$home_dir
+    sudo cp run.sh /usr/local/bin/$gnukoryavov_name
     
     echo "Cкрипт gnu-koryavov установлен."
+fi
+
+read -p "Хотите ли Вы использовать готовый скрипт просмотра Корявника? (Д/н): " ans
+if [[ $ans == "y"* || $ans == "Y"* || $ans == "д"* || $ans == "Д"* ]]; then
+
+    cp -r openscripts/ ~/$home_dir/
+    
+    #okular --version > /dev/null
+    #sed -i '/pdfviewer_script/s/=.*\.sh/='$(pdfviewer)'.sh/' test.conf
+
 fi
