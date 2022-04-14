ea4cfa6e87262958e7b56c0a87d396caf6e08aa7
sin-diesel <sidelnikov.si@phystech.edu>
2022-04-14 12:15:32 +0300

    Changed directories structure

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 home_dir="gnu-koryavov"
+scipts_dir="scripts"
+
 read -p "Вы уверены, что хотите установить скрипт gnu-koryavov в систему? (Д/н): " ans
 
 if [[ $ans == "y"* || $ans == "Y"* || $ans == "д"* || $ans == "Д"* ]]; then
 
     mkdir -p -v ~/$home_dir/KORYAVNIKS
     cp config.conf ~/$home_dir
-    sudo cp run.sh /usr/local/bin/$home_dir
+    sudo cp $scripts_dir/run.sh /usr/local/bin/$home_dir
     
     echo "Cкрипт gnu-koryavov установлен."
 fi
