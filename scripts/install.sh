09ea226be8cbb0143ad3b1ed5b5785bf208b9e20
sin-diesel <sidelnikov.si@phystech.edu>
2022-04-16 13:21:35 +0300

    Add check for os version

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,11 @@ scipts_dir="scripts"
 
 read -p "Are you sure you want to install gnu-koryavov on yout system (Y/N): " ans
 
+echo "Determining OS version..."
+cat /etc/*release
+os_verison=$(cat /etc/*release)
+echo "$os_version"
+
 if [[ $ans == "y"* || $ans == "Y"*  ]]; then
 
     mkdir -p -v ~/$home_dir/KORYAVNIKS
