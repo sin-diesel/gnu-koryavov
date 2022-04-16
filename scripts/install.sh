f63ec99932be1136b01bfea404e652b49dad9c35
sin-diesel <sidelnikov.si@phystech.edu>
2022-04-16 13:31:26 +0300

    Add atril instead of okular if ubuntu detected

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,23 @@
 home_dir="gnu-koryavov"
 scipts_dir="scripts"
 
+ubuntu_default_editor="atril"
+
 read -p "Are you sure you want to install gnu-koryavov on yout system (Y/N): " ans
 
 echo "Determining OS version..."
-cat /etc/*release
-os_verison=$(cat /etc/*release)
-echo "$os_version"
+os_info=$(cat /etc/*release)
+echo "$os_info"
+
+is_ubuntu=$(echo $os_info | grep -i "ubuntu")
+
+if [[ $? -eq 0 ]] ; then
+
+    echo "Running on ubuntu machine. Default editor: $ubuntu_default_editor"
+    sed -i "s/okular.sh/atril.sh/g" ../config.conf
+    cat ../config.conf
+
+fi 
 
 if [[ $ans == "y"* || $ans == "Y"*  ]]; then
 
