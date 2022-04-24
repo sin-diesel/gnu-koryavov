b658e10bae5085eac97b216691be96c70d61c2d0
nerett <pt-mvb@inbox.lv>
2022-04-24 21:57:38 +0300

    added more uninstallation info, fixed aborting on rm failure

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 
 home_dir=".gnu-koryavov"
-scipts_dir="scripts"
 gnukoryavov_name="gnu-koryavov"
 tmp_dir="gnu-koryavov"
+
+echo "The following files will be deleted:"
+echo "$HOME/$home_dir"
+echo "/usr/local/bin/$gnukoryavov_name"
+echo "/tmp/$tmp_dir"
+echo ""
 
 read -p "Are you sure you want to remove gnu-koryavov from your system? (Y/n): " ans
 if [[ $ans == "y"* || $ans == "Y"*  ]]; then
 
-	rm -rf $HOME/$home_dir
+	rm -r $HOME/$home_dir
 	rm -rf /tmp/$tmp_dir
 	sudo rm /usr/local/bin/$gnukoryavov_name
 
