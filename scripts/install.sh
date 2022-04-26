8badd46b171956b189ce490610b8ef5295b379a6
nerett <pt-mvb@inbox.lv>
2022-04-27 00:29:50 +0300

    changed intallation order, fixed exiting on no-editor, added more user info

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,61 +2,68 @@
 
 home_dir="gnu-koryavov"
 scipts_dir="scripts"
-gnukoryavov_name=gnu-koryavov
+gnukoryavov_name="gnu-koryavov"
 dependencies=(curl egrep sed wget)
+default_editors=(okular atril evince xreader zathura)
 
-ubuntu_default_editor="atril"
+#ubuntu_default_editor="atril"
+
 
 read -p "Are you sure you want to install gnu-koryavov on your system? (Y/n): " ans
+if [[ $ans != "y"* || $ans != "Y"*  ]]; then
+    echo "Exiting..."
+    exit 0
+fi
 
 
-editors=(okular atril evince xreader zathura)
-editor="NO EDITOR"
+echo "Checking dependencies..."
+for utility in ${dependencies[*]}; do
 
-for item in ${editors[*]}; do
-
-    $item --version &> /dev/null
-    if [[ $? == 0 ]]; then
-        editor=$item
-        break
+    $utility --version &> /dev/null
+    if [[ $? != 0 ]]; then
+        echo "$utility is not installed!"
+        exit 1
     fi
 
 done
 
-if [[ $editor == "NO EDITOR" ]]; then
 
-    echo "No supported DjVU viewer intalled"
-    exit 0
-fi
-
-echo "Choosen editor: $editor. You can change it as wrote in README"
-sed -i "/djvuviewer_script/s/=.*\.sh/=~\/gnu-koryavov\/$editor.sh/" ../config.conf
-
-
-echo $HOME
-
+read -p "Are you going to use one of the default document viewer scripts? (Y/n): " ans
 if [[ $ans == "y"* || $ans == "Y"*  ]]; then
 
-    mkdir -p -v $HOME/$home_dir/KORYAVNIKS
-    cp ../config.conf $HOME/$home_dir
-    sudo cp run.sh /usr/local/bin/$gnukoryavov_name
+    cp ../editors/* $HOME/$home_dir/
 
-    for utility in ${dependencies[*]}; do
+    editor="NO EDITOR"
 
-        $utility --version &> /dev/null
-        if [[ $? != 0 ]]; then
-            echo "$utility is not installed!"
-            exit 1
+    for item in ${default_editors[*]}; do
+
+        $item --version &> /dev/null
+        if [[ $? == 0 ]]; then
+            editor=$item
+
+            read -p "Found $editor installed. Do you want to stop searching? (Y/n): " ans
+            if [[ $ans != "y"* || $ans != "Y"*  ]]; then
+                break
+            fi
         fi
 
     done
 
-    read -p "Are you going to use one of the default document viewer scripts? (Y/n): " ans
-    if [[ $ans == "y"* || $ans == "Y"*  ]]; then
-        cp ../editors/* $HOME/$home_dir/
+    if [[ $editor == "NO EDITOR" ]]; then
+        echo "No supported DjVU viewer intalled. Proceeding with default value"
     fi
-    
-    echo "gnu-koryavov successfully installed."
-
+    else
+        echo "Choosen djvu viewer: $editor. You can change it at any time (for more information see README)"
+        sed -i "/djvuviewer_script/s/=.*\.sh/=~\/gnu-koryavov\/$editor.sh/" ../config.conf
+    fi
 fi
+
+#echo $HOME
+
+
+mkdir -p -v $HOME/$home_dir/KORYAVNIKS
+cp -v ../config.conf $HOME/$home_dir
+sudo cp -v run.sh /usr/local/bin/$gnukoryavov_name
+
+echo "gnu-koryavov successfully installed."
 
