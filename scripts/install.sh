30bbbc4b67d18e9931058bf47a06205d6ccf6222
Maxmosk <maxmosk@list.ru>
2022-05-07 12:55:19 +0300

    [Bugfix] Fixed typo in path

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
+main_config=../configs/config.conf
 #!/bin/bash
 set -uo pipefail
 
 home_dir="gnu-koryavov"
 scipts_dir="scripts"
 gnukoryavov_name=gnu-koryavov
+main_config=../configs/config.conf
 
 read -p "Are you sure you want to install gnu-koryavov on your system? (Y/n): " ans
 
