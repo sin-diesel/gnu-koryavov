61069a26b104545afe2e2c6bcd0cf6f9387b8875
Maxmosk <maxmosk@list.ru>
2022-05-09 16:31:10 +0300

    [Improvement] Only necessary actions in preinst

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,26 +6,49 @@ source $install_config
 
 
 read -p "Are you sure you want to install gnu-koryavov on your system? (Y/n): " ans
+if [[ $ans != "y"* && $ans != "Y"*  ]]; then
+    echo "Exiting..."
+    exit 0
+fi
 
 
-echo $HOME
+bash ./preinst.sh
 
+echo "Installing gnu-koryavov..."
+mkdir -p -v $HOME/$home_dir/KORYAVNIKS
+sudo cp -v run.sh /usr/local/bin/$gnukoryavov_name
+
+
+read -p "Are you going to use one of the default document viewer scripts? (Y/n): " ans
 if [[ $ans == "y"* || $ans == "Y"*  ]]; then
 
-    mkdir -p -v $HOME/$home_dir/KORYAVNIKS
-    cp $main_config $HOME/$home_dir
-    sudo cp run.sh /usr/local/bin/$gnukoryavov_name
+    cp -v ../editors/* $HOME/$home_dir/
 
-    bash ./preinst.sh
-    if [[ $? != 0 ]]; then
-        echo "Installation is not certified due to lack of dependencies"
+    viewer="NO VIEWER"
+
+    for item in ${default_viewers[*]}; do
+
+        $item --version &> /dev/null
+        if [[ $? == 0 ]]; then
+            viewer=$item
+
+            read -p "Found $viewer installed. Do you want to stop searching? (Y/n): " ans
+            if [[ $ans != "y"* || $ans != "Y"*  ]]; then
+                break
+            fi
+        fi
+
+    done
+
+    if [[ $viewer == "NO VIEWER" ]]; then
+        echo "No supported DjVU viewer installed (see README for more information). Proceeding with default value"
+    else
+        echo "Choosen djvu viewer: $viewer. You can change it at any time (for more information see README)"
+        sed -i "/djvuviewer_script/s/=.*\.sh/=~\/gnu-koryavov\/$viewer.sh/" $main_config
     fi
-
-    read -p "Are you going to use one of the default document viewer scripts? (Y/n): " ans
-    if [[ $ans == "y"* || $ans == "Y"*  ]]; then
-        cp ../editors/* $HOME/$home_dir/
-    fi
-
-    echo "gnu-koryavov successfully installed."
-
 fi
+
+
+cp -v $main_config $HOME/$home_dir
+echo "gnu-koryavov successfully installed."
+
--- scripts/preinst.sh
+++ scripts/preinst.sh
@@ -5,6 +5,7 @@ install_config=../configs/install.conf
 source $install_config
 
 
+echo "Checking dependencies..."
 for utility in ${dependencies[*]}; do
 
     $utility --version &> /dev/null
@@ -12,28 +13,8 @@ for utility in ${dependencies[*]}; do
         echo "$utility is not installed!"
         exit 1
     fi
+
 done
-
-
-editor="NO EDITOR"
-
-for item in ${editors[*]}; do
-    $item --version &> /dev/null
-    if [[ $? == 0 ]]; then
-        editor=$item
-        break
-    fi
-done
-
-if [[ $editor == "NO EDITOR" ]]; then
-
-    echo "No supported DjVU viewer intalled"
-    exit 2
-fi
-
-echo "Choosen editor: $editor. You can change it as wrote in README"
-sed -i "/djvuviewer_script/s/=.*\.sh/=~\/gnu-koryavov\/$editor.s    h/" $main_config
-
 
 exit 0
 
